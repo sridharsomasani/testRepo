e0f9061b0c6323822f09d7c106ac13d7b800968f
Sridhar Somasani <sridhar.somasani@gmail.com>
2018-04-18 00:12:49 -0500

    Added versioning for controller Refactor code as per REST best practices Added Status responses

--- controller/UserServiceController.java
+++ controller/UserServiceController.java
@@ -1,22 +1,24 @@
 package com.outdoor.buddies.controller;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.outdoor.buddies.jpa.entity.UserProfile;
 import com.outdoor.buddies.repository.UserProfileRepository;
 
 @RestController
-@RequestMapping(value="/api/user")
-public class UserServiceController {
+@RequestMapping(value="/api/v1/users")
+public class UserServiceController{
 	
 	@Autowired
 	private UserProfileRepository userProfileRepository;
@@ -26,20 +28,45 @@ public class UserServiceController {
 		return "Authenticated";
 	}
 	
-	@RequestMapping(value="/register", method= RequestMethod.POST)
-	public UserProfile registerUser(@RequestBody UserProfile user) {
+	@RequestMapping(method= RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public UserProfile createUser(@RequestBody UserProfile user) {
+		return userProfileRepository.save(user);
+		
+	}
+		
+	@RequestMapping(value="/{userId}", method= RequestMethod.PUT)
+	public UserProfile updateUser(@RequestBody UserProfile user, @PathVariable("userId") Long userId) {
 		return userProfileRepository.save(user);
 		
 	}
 	
-	@RequestMapping(value="/searchByUsername", method= RequestMethod.GET)
-	public UserProfile searchUsers(@RequestParam String userName) {
+	@RequestMapping(value="/{userId}", method= RequestMethod.GET)
+	public Optional<UserProfile> findUser(@PathVariable("userId") Long userId ) {
+		return userProfileRepository.findById(userId);
+	}
+	
+	@RequestMapping(value="/{userId}", method= RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteUser(@PathVariable("userId") Long userId ) {
+		userProfileRepository.deleteById(userId);
+		return;
+	}
+	
+	@RequestMapping(method= RequestMethod.PUT)
+	public Iterable<UserProfile> updateUsers(@RequestBody List<UserProfile> users) {
+		return userProfileRepository.saveAll(users);
+		
+	}
+	
+	@RequestMapping(value="/username/{uName}", method= RequestMethod.GET)
+	public UserProfile findByUserName(@PathVariable("uName") String userName) {
 		return userProfileRepository.findByUserName(userName);
 	}
 	
-	@RequestMapping(value="/{userId}", method= RequestMethod.GET)
-	public Optional<UserProfile> findUser(@PathVariable("userId") long userId ) {
-		return userProfileRepository.findById(userId);
+	@RequestMapping(value="/email/{param}", method= RequestMethod.GET)
+	public UserProfile findByEmail(@PathVariable("param") String emailId) {
+		return userProfileRepository.findByEmailId(emailId);
 	}
 	
 }
