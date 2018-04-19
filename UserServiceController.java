91b4d0385b49ce814fdc9332d8b90c2a0da1769f
Sridhar Somasani <sridhar.somasani@gmail.com>
2018-04-19 01:49:30 -0500

    Added Valid annotation to validate entities mapped from RequestBody
    Refactored code to use UserService
    Place holders for handling error for controllers

--- UserServiceController.java
+++ UserServiceController.java
@@ -1,7 +1,6 @@
 package com.outdoor.buddies.controller;
 
-import java.util.List;
-import java.util.Optional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,14 +13,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.outdoor.buddies.jpa.entity.UserProfile;
-import com.outdoor.buddies.repository.UserProfileRepository;
+import com.outdoor.buddies.service.UserService;
 
 @RestController
 @RequestMapping(value="/api/v1/users")
 public class UserServiceController{
 	
 	@Autowired
-	private UserProfileRepository userProfileRepository;
+	private UserService userProfileService;
 
 	@RequestMapping(value="/authenticate", method= RequestMethod.GET)
 	public String authenticateUser(@RequestParam("username") String username, @RequestParam("passwd") String passwd ) {
@@ -30,43 +29,61 @@ public class UserServiceController{
 	
 	@RequestMapping(method= RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public UserProfile createUser(@RequestBody UserProfile user) {
-		return userProfileRepository.save(user);
+	public UserProfile createUser(@RequestBody @Valid UserProfile user) {
+		return userProfileService.createUser(user);
 		
 	}
 		
 	@RequestMapping(value="/{userId}", method= RequestMethod.PUT)
-	public UserProfile updateUser(@RequestBody UserProfile user, @PathVariable("userId") Long userId) {
-		return userProfileRepository.save(user);
+	public UserProfile updateUser(@RequestBody @Valid UserProfile user, @PathVariable("userId") Long userId) {
+		UserProfile dbUser = userProfileService.updateUser(user, userId);
+		if(dbUser == null) {
+			//handle with appropriate HTTP status code and message
+			return null;
+		}
+		return dbUser;
 		
 	}
 	
 	@RequestMapping(value="/{userId}", method= RequestMethod.GET)
-	public Optional<UserProfile> findUser(@PathVariable("userId") Long userId ) {
-		return userProfileRepository.findById(userId);
+	public UserProfile findUser(@PathVariable("userId") Long userId ) {
+		UserProfile dbUser = userProfileService.findUser(userId);
+		if(dbUser == null) {
+			//handle with appropriate HTTP status code and message
+			return null;
+		}
+		return dbUser;
 	}
 	
 	@RequestMapping(value="/{userId}", method= RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteUser(@PathVariable("userId") Long userId ) {
-		userProfileRepository.deleteById(userId);
-		return;
-	}
-	
-	@RequestMapping(method= RequestMethod.PUT)
-	public Iterable<UserProfile> updateUsers(@RequestBody List<UserProfile> users) {
-		return userProfileRepository.saveAll(users);
-		
+	public UserProfile deleteUser(@PathVariable("userId") Long userId ) {
+		UserProfile dbUser = userProfileService.deleteUser(userId);
+		if(dbUser == null) {
+			//handle with appropriate HTTP status code and message
+			return null;
+		}
+		return dbUser;
 	}
 	
 	@RequestMapping(value="/username/{uName}", method= RequestMethod.GET)
 	public UserProfile findByUserName(@PathVariable("uName") String userName) {
-		return userProfileRepository.findByUserName(userName);
+		UserProfile dbUser = userProfileService.findByUserName(userName);
+		if(dbUser == null) {
+			//handle with appropriate HTTP status code and message
+			return null;
+		}
+		return dbUser;
 	}
 	
 	@RequestMapping(value="/email/{param}", method= RequestMethod.GET)
 	public UserProfile findByEmail(@PathVariable("param") String emailId) {
-		return userProfileRepository.findByEmailId(emailId);
+		UserProfile dbUser = userProfileService.findByEmail(emailId);
+		if(dbUser == null) {
+			//handle with appropriate HTTP status code and message
+			return null;
+		}
+		return dbUser;
 	}
 	
 }
