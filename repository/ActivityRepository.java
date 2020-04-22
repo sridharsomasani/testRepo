027051c6d3a209e7488795232c8f8524d51eb02e
Sridhar Somasani <sridhar.somasani@gmail.com>
2020-04-22 00:04:21 -0500

    moved files

--- repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.outdoor.buddies.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.outdoor.buddies.jpa.entity.Activity;
-
-public interface ActivityRepository extends CrudRepository<Activity, Long>{
-
-}
